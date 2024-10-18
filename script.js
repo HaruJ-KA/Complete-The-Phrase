d31152371e0865d0493a814309a086bb91502098
Haru <j.li77@share.epsb.ca>
2024-10-18 15:13:19 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById("btn1").addEventListener("click", insult);
+
+function insult() {
+  alert("To injury.");
+}
+
+document.getElementById("btn2").addEventListener("click", birds);
+
+function birds() {
+  alert("Flock together.");
+}
+
+document.getElementById("btn3").addEventListener("click", act);
+
+function act() {
+  alert("Together.");
+}
+
+document.getElementById("btn4").addEventListener("click", beat);
+
+function beat() {
+  alert("The bush.");
+}
+
+document.getElementById("btn5").addEventListener("click", skin);
+
+function skin() {
+  alert("Of your teeth.");
+}
